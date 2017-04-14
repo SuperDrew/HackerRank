01af5326051a291b896a7d5e2b5150311144da5d
SuperDrew <a.vector@gmail.com>
2017-04-14 22:40:59 +0100

    Circle collide initial commit, test with basic example in, no implementation yet

---
+++ 31/Colliding Circles/Collide.cs
@@ -0,0 +1,11 @@
+﻿namespace _31.Colliding_Circles
+{
+    public static class Collide
+    {
+        public static float Go(int numberOfCircles, int numberOfSeconds, int[] radii)
+        {
+
+            return 1.0f;
+        }
+    }
+}
---
+++ 31/Colliding Circles/CollideTest.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+
+namespace _31.Colliding_Circles
+{
+    [TestFixture]
+    public class CollideTest
+    {
+        [Test]
+        [TestCase(3, 1, new[] { 1, 2, 3 }, 67.0206432766f, TestName = "n3, k1, radii: 1, 2, 3: expected result 67.02...")]
+        public void Go(int numberOfCircles, int numberOfSeconds, int[] radii, float expectedRadius)
+        {
+            var radius = Collide.Go(numberOfCircles, numberOfSeconds, radii);
+            Assert.That(radius, Is.EqualTo(expectedRadius).Within(1).Ulps);
+        }
+    }
+}
